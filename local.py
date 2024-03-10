4108581dc9a0adca8462fdf836c0a490aa8b1e2e
Moon-Touched <dino1997@qq.com>
2024-03-10 08:01:57 +0800

    。

--- local.py
+++ local.py
@@ -5,13 +5,13 @@ import asyncio
 scraper = Scraper(time_interval=3)
 manager = Movie_Manager(scraper=scraper, movie_path="E:\\啊啊啊", capture_path="E:\\啊啊啊\\to be capture")
 
-#scraper.get_actor_all_movie_info("神木麗")
+# scraper.get_actor_all_movie_info("神木麗")
 # task_list = manager.get_magnet_list(movie_query_set=Movie.objects(actors="神木麗"))
-# asyncio.run(manager.save_to_pikpak(task_list))
+# asyncio.run(manager.save_to_pikpak(task_list,))
 
-# actors=Actor.objects()
-# for actor in actors:
-#     actor_name=actor.name
-#     scraper.get_actor_all_movie_info(actor_name)
+actors = Actor.objects()
+for actor in actors:
+    actor_name = actor.name
+    scraper.get_actor_all_movie_info(actor_name)
 
-manager.match_info()
+# manager.match_info()
